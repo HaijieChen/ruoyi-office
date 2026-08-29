8f9bdec661cc5932ac9b6b592e89c3fa7998591e
RuoYi Office <289957361@qq.com>
2026-08-29 17:29:00 +0800

    fix(hrm): lazy-inject PermissionApi to break startup circular reference
    Test jar failed to boot: permissionApiImpl wrapped after employeeServiceImpl raw inject.

--- yudao-module-hrm/yudao-module-hrm-server/src/main/java/cn/iocoder/yudao/module/hrm/service/employee/EmployeeServiceImpl.java
+++ yudao-module-hrm/yudao-module-hrm-server/src/main/java/cn/iocoder/yudao/module/hrm/service/employee/EmployeeServiceImpl.java
@@ -28,6 +28,7 @@ import cn.iocoder.yudao.module.system.enums.DictTypeConstants;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -120,6 +121,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     private AdminUserApi adminUserApi;
 
     @Resource
+    @Lazy
     private PermissionApi permissionApi;
 
     @Resource
